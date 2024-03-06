e5a1095cb8293ae8a56e7dad9d2b5ecc7f57cad5
Saylaubay <saylaubaybekmurzaev@gmail.com>
2024-03-06 22:30:16 +0500

    #2

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/",function (){
+   return "Home page";
+});
+Route::get("/test",function (){
+   return "TEST";
+});
